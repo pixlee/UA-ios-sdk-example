4fe2bf8752a4af37d9471e0897346eb7b3de4175
Mohamed Rachidi <mo@mohamed-rachidi.local>
2017-03-28 15:10:15 -0700

    carthage 0.02

---
+++ Example/Carthage/Checkouts/UA-ios-sdk-example/Example/pixlee-ios-sdk/PXLAppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  PXLAppDelegate.m
+//  pixlee-ios-sdk
+//
+//  Created by CocoaPods on 04/30/2015.
+//
+
+#import "PXLAppDelegate.h"
+
+#import <pixlee_ios_sdk/PXLClient.h>
+#import <pixlee_ios_sdk/PXLAlbum.h>
+
+
+@implementation PXLAppDelegate
+
+#warning Replace with your Pixlee API key.
+static NSString * const PXLClientAPIKey = @"zk4wWCOaHAo4Hi8HsE";
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[PXLClient sharedClient] setApiKey:PXLClientAPIKey];
+    return YES;
+}
+
+@end
